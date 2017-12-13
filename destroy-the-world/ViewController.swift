a18f24597b8cd48dc629d5f8faa8b7c62190f7b5
Tom Nguyen <tomriddle1403@gmail.com>
2017-12-12 23:49:28 -0600

    Initial Commit

---
+++ destroy-the-world/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  destroy-the-world
+//
+//  Created by Tom Riddle on 12/12/17.
+//  Copyright © 2017 Tom Riddle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
